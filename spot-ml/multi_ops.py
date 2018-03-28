ccf2bd0a8ae97f0f27290d8807ce8e510ee6ec75
Lollipopsaurus <den.kilgore@gmail.com>
2018-03-28 11:26:30 -0400

    updates to multi_ops scripts for ml and oa

--- spot-ml/multi_ops.py
+++ spot-ml/multi_ops.py
@@ -1,27 +1,27 @@
-#!/bin/python
-
-import datetime
+mport datetime
 import os
 import subprocess
 import sys
 
 def run_process(date_stamp, ml_type):
-    command = "./ml_ops_cdh.sh " + date_stamp + " " + ml_type
-    process = subprocess.call(command)
+    command = "./ml_ops_cdh.sh"
+    process = subprocess.call([command, date_stamp, ml_type])
 
 def main():
     start_date_stamp = sys.argv[1]
     ml_type = sys.argv[2]
 
-    if sys.argv[3]:
+    if len(sys.argv) == 4:
         start_date = datetime.datetime.strptime(start_date_stamp,"%Y%m%d")
         end_date = datetime.datetime.strptime(sys.argv[3], "%Y%m%d")
         delta = end_date - start_date
         for x in range (0, delta.days):
-            print x
             date_to_run = start_date + datetime.timedelta(days=x)
             new_datestamp = date_to_run.strftime("%Y%m%d")
             run_process(new_datestamp, ml_type)
+
+    else:
+        run_process(start_date_stamp, ml_type)
 
 if __name__ == '__main__':
     main()
---
+++ spot-oa/oa/multi_ops.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+import datetime
+import os
+import subprocess
+import sys
+
+def run_process(date_stamp, ml_type, tolerance):
+    command = "start_oa.py"
+    process = subprocess.call(['python', command, '-d', date_stamp, '-t', ml_type, '-l', tolerance])
+
+def main():
+    start_date_stamp = sys.argv[1]
+    ml_type = sys.argv[2]
+    tolerance = sys.argv[3]
+
+    if len(sys.argv) == 5:
+        start_date = datetime.datetime.strptime(start_date_stamp,"%Y%m%d")
+        end_date = datetime.datetime.strptime(sys.argv[4], "%Y%m%d")
+        delta = end_date - start_date
+        for x in range (0, delta.days):
+            date_to_run = start_date + datetime.timedelta(days=x)
+            new_datestamp = date_to_run.strftime("%Y%m%d")
+            run_process(new_datestamp, ml_type, tolerance)
+    else:
+         run_process(start_date_stamp, ml_type, tolerance)
+
+
+if __name__ == '__main__':
+    main()
